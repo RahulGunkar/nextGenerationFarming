ce2344cc362906cce2dd08dd9a4cce955245f812
Rahul Gunkar <rgunkar@yahoo.in>
2018-11-17 13:14:43 +0530

    Add files via upload

---
+++ hack.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+   return render_template('homePage.html')
+
+@app.route('/result',methods = ['POST', 'GET'])
+def result():
+   if request.method == 'POST':
+      results = username = request.form['userInput']
+      if results == '1':
+      	sell = True
+      	return render_template("sellCrop.html",sell = sell)
+
+
+@app.route('/sell',methods = ['POST', 'GET'])  
+def sell():
+   if request.method == 'POST':
+      results = username = request.form['userInput']
+      if results == '1':
+      	sell = True
+      	return render_template("sellCrop.html",sell = sell)  
+
+
+@app.route('/service',methods = ['POST', 'GET'])  
+def service():
+   if request.method == 'POST':
+      results = username = request.form['userInput']
+      if results == '1':
+      	sell = True
+      	return render_template("sellCrop.html",sell = sell)  
+
+
+if __name__ == '__main__':
+   app.run(debug = True)
