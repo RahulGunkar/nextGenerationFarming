bc61441ebfdaf6434df54c5be85e2f091aeba012
Rahul Gunkar <rgunkar@yahoo.in>
2018-11-17 13:19:00 +0530

    Update hack.py

--- hack.py
+++ hack.py
@@ -3,8 +3,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-   return render_template('homePage.html')
-
+   return "hiii"
+   #return render_template('homePage.html')
+'''
 @app.route('/result',methods = ['POST', 'GET'])
 def result():
    if request.method == 'POST':
@@ -31,6 +32,6 @@ def service():
       	sell = True
       	return render_template("sellCrop.html",sell = sell)  
 
-
+'''
 if __name__ == '__main__':
    app.run(debug = True)
